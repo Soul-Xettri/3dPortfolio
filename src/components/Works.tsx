35284becf6bfe70db5098e915212bc252005b0eb
sou-xettri <rakeshchhetri420@gmail.com>
2024-04-03 10:22:01 +0545

    Add project cards and section wrapper to Works component

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,7 +1,107 @@
-const Works = () => {
-  return (
-    <div>Works</div>
-  )
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { github } from "../assets";
+import { SectionWrapper } from "../hoc";
+import { projects } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+interface ProjectCardProps {
+  index: number;
+  name: string;
+  description: string;
+  tags: { name: string; color: string }[];
+  image: string;
+  source_code_link: string;
 }
 
-export default Works
+const ProjectCard = ({
+  index,
+  name,
+  description,
+  tags,
+  image,
+  source_code_link,
+}: ProjectCardProps) => {
+  return (
+    <motion.div
+      variants={fadeIn({
+        direction: "up",
+        type: "spring",
+        delay: index * 0.5,
+        duration: 0.75,
+      })}
+    >
+      <Tilt
+        options={{ max: 45, scale: 1, speed: 450 }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] max-w-full"
+      >
+        <div className="relative w-full h-[230px]">
+          <img
+            src={image}
+            alt={name}
+            className="w-full h-full object-cover rounded-2xl"
+          />
+          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+            <div
+              onClick={() => window.open(source_code_link, "_blank")}
+              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+            >
+              <img
+                src={github}
+                alt="github"
+                className="w-1/2 h-1/2 object-contain"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="mt-5">
+          <h3 className="text-white font-bold text-[24px]">{name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+        </div>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
+    </motion.div>
+  );
+};
+
+const Works = () => {
+  return (
+    <>
+      <motion.div variants={textVariant({})}>
+        <p className={styles.sectionSubText}>My work</p>
+        <p className={styles.sectionHeadText}>Projects.</p>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn({
+            direction: "",
+            type: "",
+            delay: 0.1,
+            duration: 1,
+          })}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Following Projects showcase my skills and experience in web
+          development. I have worked on both front-end and back-end technologies
+          to build these projects. It reflects my ability to solve problems and
+          create user-friendly interfaces.
+        </motion.p>
+      </div>
+      <div className="mt-20 flex flex-wrap gap-7">
+        {projects.map((project, index) => (
+          <ProjectCard key={`project-${index}`} index={index} {...project} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper({ Component: Works, idName: "works" });
